83063f518e07580be63de80e44ce4c2f007df60b
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2022-12-03 22:52:08 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+const listArray = [];
+
+function createList() {
+for (let i = 0; i < ingredients.length; i +=1) {
+  const li = document.createElement('li');
+  li.textContent = ingredients[i];
+  li.classList.add('item');
+  listArray.push(li);
+  }
+  list.append(...listArray);
+};
+
+createList();
