186fc483b40b9399204326da3a6a0dff156eba88
Spartak Maznytskyi <spartakm@ukr.net>
2022-12-04 15:42:37 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const btnChangeColor = document.querySelector(".change-color");
+btnChangeColor.addEventListener("click", bodyChangeColor);
+const colorValue = document.querySelector(".color");
+const body = document.querySelector("body");
+const style = document.querySelector("style");
+
+let bodyColor = "";
+
+function bodyChangeColor() {
+  colorValue.textContent = getRandomHexColor();
+  bodyColor = `body{background-color: ${getRandomHexColor()};}`;
+  style.insertAdjacentHTML("beforeend", bodyColor);
 }
