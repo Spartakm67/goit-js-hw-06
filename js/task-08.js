9a621baaf8dbdd4e322624801a88553b313b5b92
Spartak Maznytskyi <spartakm@ukr.net>
2022-12-04 14:21:46 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all the fields!");
+  }
+
+  const formElements = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(formElements);
+  //   console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
